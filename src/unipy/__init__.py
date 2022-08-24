3cbf60bf5faa503d7b0527214c14fc8febbe66e5
Daryl Stark <daryl@dstark.nl>
2022-08-24 22:23:28 +0200

    Added DOCSTRING to the initiator

--- src/unipy/__init__.py
+++ src/unipy/__init__.py
@@ -1,1 +1,5 @@
-from unipy.unipy import *
+""" Library to interact with UnifiOS devices """
+
+from unipy.unipy import Unipy
+from unipy.network import Network
+from unipy.unipyconnection import UnipyConnection
